62006deb9d59513db9d7dac64282e9057355ae43
petefrans2024-hub <petefrans2024@gmail.com>
2026-06-24 21:51:59 -0400

    Fix TS type cast

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -13,7 +13,7 @@ export const supabase = new Proxy({} as SupabaseClient, {
       }
       _supabase = createClient(url, key);
     }
-    return (_supabase as Record<string, unknown>)[prop as string];
+    return (_supabase as unknown as Record<string, unknown>)[prop as string];
   },
 });
 
